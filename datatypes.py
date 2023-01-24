8b9e0582da288750adfe4a0199ef57fe1f3f2e9b
Nasir Mustapha <teboy.com@gmail.com>
2023-01-24 12:11:05 +0100

    more on datatypes

--- datatypes.py
+++ datatypes.py
@@ -1,8 +1,66 @@
 "Abubakar Galadima"
+'Abdulwahb'
+"Zainab"
+"1234"
+" "
+""
 1
+-1
 1.1
+-2.5
 True
 False
+["", 1, 1.4, True, [1, "1"]]
 [1, 2.1, ["Abdubalar"]]
 (1, 2.1, (1, 2, [1, 2]), True)
-{"name":"Abubakar", "age":13}
+(1, 2, [], ())
+
+{"name":"Abubakar", "age":10, "friend":["Abu", "Bakar"], "hobbies":("Swimming", "Reading")}
+
+# Integers
+1
+-20
+# Tuple
+()
+(2, [])
+# Boolean
+True
+False
+# Float
+2.2
+3.4
+-5.4
+# List
+[1.2, True, ()]
+# Dictionary
+{"name":'Musa', "age":35}
+# Strings
+"abubakar"
+'aisha'
+
+# Float
+0.5
+5.2
+3.0
+-3.5
+# Tuple
+("aisha", 5, True)
+# String
+"Aliyu"
+"20"
+"$100"
+"aisha=1%$_+-&*%sdjhgdfjsbdnmfbsdjbfnjs"
+# List
+[35, "musa", False]
+# Tuple
+(1, [], {})
+
+# Exercise
+# Using 5 examples for each, 
+# explain all the basic data types in python.
+
+# STRING
+"Aisha"
+"Abubakar"
+
+# FLOAT
---
+++ dictionaries.py
@@ -0,0 +1,73 @@
+# dictionaries are contained within
+# curly braces
+person = {"name":"Abdulwahab Hassan", "age":15, "hobbies":["Swimming", 'Eating']}
+
+print(person)
+
+person.clear()
+print(person)
+
+person2 = person.copy()
+
+print(person2)
+
+person3 = {"name":"Aisha Auwal", "age":35, "friend":"Shamsudeen Abdullahi"}
+print(person3.get("name"))
+print(person3.get("age"))
+print(person3.get("friend"))
+
+person4 = {"name":"Khauthar Yusuf", "age":3}
+print(person4.get("name"))
+print(person4.get("age"))
+
+print(person4.items())
+print(person4.keys())
+
+person4.pop("name")
+person4.pop("age")
+
+print(person4)
+
+person5={"name":"Aisha Auwal", "complexion":"brown"}
+print(person5.get("complexion"))
+print(person5.get("name"))
+
+print(person5.popitem())
+print(person5)
+
+person5.setdefault("age", 35)
+
+print(person5)
+
+person5.setdefault("friend", "Shamsuddeen")
+
+print(person5)
+
+person5.setdefault("dob", "01-01-2005")
+print(person5)
+person5.setdefault("dob", "01-01-2009")
+print(person5)
+
+person5.update({"dob":"01-01-2009"})
+
+print(person5)
+
+person5.update({"name":"Abubakar Hassan"})
+
+print(person5)
+
+person5.update({"age":14})
+
+print(person5)
+
+person5.update({"friend":"Jamil Salis"})
+
+print(person5)
+
+print(person5.get("age"))
+print(person5.get("dob"))
+print(person5.get("name"))
+
+person5.pop("age")
+
+print(person5)
