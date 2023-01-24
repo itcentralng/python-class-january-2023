abef00ab75459211adb4bbe219c9b979c753206f
Nasir Mustapha <teboy.com@gmail.com>
2023-01-24 12:11:10 +0100

    assignment

---
+++ assignment.py
@@ -0,0 +1,15 @@
+# Create a folder on the root of this folder with your name
+# Create a file called assignment.py within the created folder
+# And answer the following questions, submission on Monday 23rd of January, 2023
+
+# Q1: Using comments, list all the data types you know with examples
+# Q2: Read about variable naming convention in python and create examples of each
+# Q3: List 5 String methods with an example using print() in each case to print your result
+# Q4: List 5 List methods with an example using print() in each case to print your result
+# Q5: Create an a list with dictionaries with the following information
+#      Name, Age, Class, Subjects
+#      each subject should have CA and Exam score
+#      use console to print the following statement for at least two of the students.
+#      NAME -----  AGE  ------  CLASS  ------ TOTAL
+#      James ----- 10 --------  Jss One ----- 150.5
+#      NOTE: there should be atleast 3 students and atleast 5 subjects each student.
