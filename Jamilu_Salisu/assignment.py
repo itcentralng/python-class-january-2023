98ace31b2d56c211e1cfdb6e9027c87c1c2beae9
Jamilu Salisu <jamilusalis@gmail.com>
2023-01-25 22:10:19 +0100

    list of data types with examples

---
+++ Jamilu_Salisu/assignment.py
@@ -0,0 +1,27 @@
+
+# Q1: Using comments, list all the data types you know with examples
+
+# String
+name = "Jamilu Salisu"
+
+# Integer
+age = 24
+
+# Float
+height = 1.75
+
+# Boolean
+isHealthy = True
+
+# List
+skills_learning = ["Python", "Flask", "Dart", "Flutter", "Clound Computing"]
+
+# Dictionary
+intern_profile = {"name": "Jamilu Salisu",
+                  "institution": "BUK", "department": "SWE", "level": 300, "duration": "6 months"}
+
+# Tuple
+skills = ("PHP", "Javascript", "HTML", "CSS", "Angurlar", "Flutter")
+
+# Set
+hobbies = {"Coding", "Watching Anime", "Reading Comics", "Networking"}
