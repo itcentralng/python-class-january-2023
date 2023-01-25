d77245237cc632ec40d0cf1f5f085dd1a35d3bb5
Jamilu Salisu <jamilusalis@gmail.com>
2023-01-25 22:16:50 +0100

    variable naming convention with examples

--- Jamilu_Salisu/assignment.py
+++ Jamilu_Salisu/assignment.py
@@ -25,3 +25,21 @@ skills = ("PHP", "Javascript", "HTML", "CSS", "Angurlar", "Flutter")
 
 # Set
 hobbies = {"Coding", "Watching Anime", "Reading Comics", "Networking"}
+
+
+# Q2: Read about variable naming convention in python and create examples of each
+
+# (i) variable name must start with a letter or the underscore character
+firstname = "Jamilu"
+_othername = "Mohammed"
+
+# (ii) variable name cannot start with a number
+# 2lastname = "Salisu" # an error will occur
+
+# (iii) variable name can only contain alpha-numeric characters and underscores(A-z, 0-9, and _)
+full_name1 = "Jamilu Salisu"
+
+# (iv) variable names are case-sensitive
+height = 1.50
+Height = 1.60
+HEIGHT = 1.75
