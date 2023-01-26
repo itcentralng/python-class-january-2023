7df8deb5c557a7c92e1355eaf14fd2ec722f8946
Jamilu Salisu <jamilusalis@gmail.com>
2023-01-26 06:25:33 +0100

    a list of inters with dictionary

--- Jamilu_Salisu/assignment.py
+++ Jamilu_Salisu/assignment.py
@@ -62,3 +62,45 @@ print("I am {} years old and I earn {}$ per month.".format(
 
 # count the number of times a word appears in the list
 print("I am learning Flutter and Python.".count("am"))
+
+# Q4: List 5 List methods with an example using print() in each case to print your result
+
+
+# Q5: Create an a list with dictionaries with the following information
+#      Name, Age, Class, Subjects
+#      each subject should have CA and Exam score
+
+interns_scores = [
+    {"name": "Jamila", "age": 20, "class": "SS One",
+     "subjects": [
+         {"course": "Dart", "CA": 25, "Exam": 55},
+         {"course": "Flutter", "CA": 19, "Exam": 39},
+         {"course": "Python", "CA": 27, "Exam": 40},
+         {"course": "Flask", "CA": 30, "Exam": 52},
+         {"course": "Cloud Computing", "CA": 22, "Exam": 53}
+     ]},
+    {"name": "Abdulaziz", "age": 24, "class": "Jss Two",
+     "subjects": [
+         {"course": "Dart", "CA": 21, "Exam": 44},
+         {"course": "Flutter", "CA": 8, "Exam": 33},
+         {"course": "Python", "CA": 25, "Exam": 58},
+         {"course": "Flask", "CA": 17, "Exam": 47},
+         {"course": "Cloud Computing", "CA": 22, "Exam": 53}
+     ]},
+    {"name": "Hauwa'u", "age": 19, "class": "SS Three",
+     "subjects": [
+         {"course": "Dart", "CA": 19, "Exam": 44},
+         {"course": "Flutter", "CA": 22, "Exam": 53},
+         {"course": "Python", "CA": 27, "Exam": 38},
+         {"course": "Flask", "CA": 26, "Exam": 49},
+         {"course": "Cloud Computing", "CA": 29, "Exam": 58}
+     ]}
+]
+
+#      Calculate the total score for each student and print the result
+#      use console to print the following statement for at least two of the students.
+#      NAME -----  AGE  ------  CLASS  ------ TOTAL
+
+#      James ----- 10 --------  Jss One ----- 150.5
+
+#      NOTE: there should be atleast 3 students and atleast 5 subjects each student.
