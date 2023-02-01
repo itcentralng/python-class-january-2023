5b5e3fb250fcf93be72aecd0d527faef211cf0c9
Jamilu Salisu <jamilusalis@gmail.com>
2023-02-01 13:47:25 +0100

    game pactice

--- Jamilu_Salisu/practice2.py
+++ Jamilu_Salisu/practice2.py
@@ -12,7 +12,7 @@ stocks = [
 ]
 
 # sort the list of items by price
-# stocks.sort(key=lambda x: x.get("price"))
+#  stocks.sort(key=lambda x: x.get("price"))
 spent_limit = 50
 
 for i in stocks:
@@ -23,12 +23,42 @@ for i in stocks:
         else:
             print("Sorry, you cannot afford any of the products")
 
+print("####################\n")
+
 # Write a shopping game where a player will start by
-# setting how much they want to spend in our shop with
-# maximum spend limit of 5000 naira.
-# Players will then be presented with a list of items one
-# by one for them to choose from.
-# They must not buy more than 5 items.
-# If a player spends all their money buy the end of the game
-# they win, otherwise they can choose to play again or
-# its game over.
+# setting how much they want to spend in our shop
+spend_limit = int(input("How much do you want to spend? "))
+
+# with maximum spend limit of 5000 naira.
+if spend_limit > 5000:
+    print("Sorry, you cannot spend more than 5000 naira")
+    exit()
+
+# Players will then be presented with a list of items one by one for them to choose from.
+
+purchasable_items = [
+    {"name": "Milk", "price": 1200},
+    {"name": "Bread", "price": 600},
+    {"name": "Eggs", "price": 1500},
+    {"name": "Sugar", "price": 700},
+    {"name": "Salt", "price": 200},
+    {"name": "Rice", "price": 700},
+    {"name": "Beans", "price": 950},
+]
+
+print(purchasable_items)
+
+# They must not buy more than 5 items. If a player spends all their money buy the end of the game
+# they win, otherwise they can choose to play again or its game over.
+
+purchased_items = []
+for item in purchasable_items:
+    print("Do you want to buy", item.get("name"), "?")
+    answer = input("Yes or No: ")
+
+    if answer.lower() == "yes":
+        if item.get("price") <= spend_limit:
+            purchased_items.append(item)
+            spend_limit -= item.get("price")
+
+print(purchased_items)
