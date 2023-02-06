8736a905e598af02545984f895ef3c3e66a2e4f7
Zaina995 <xeexy99@gmail.com>
2023-02-06 11:08:58 +0100

    mine

---
+++ Zaina/assignment2.js
@@ -0,0 +1,29 @@
+// # Q1: 
+// # Declare a variable called "string" and assign it a string
+// #  value. Use the lower() method to convert "string"
+// #  to lowercase, then use the split() method to create
+// #  a list of characters from "string" and use the reverse()
+// #  method to reverse the order of the characters in 
+// # the list. Finally, join the characters in the list 
+// # back together into a string and store the result in 
+// # a new variable called "reverse_string". Compare "string" 
+// # to "reverse_string" using conditional statement and output 
+// # "The string is a palindrome" if they are the same or 
+// # "The string is not a palindrome" if they are different.
+
+string = "AISHA"
+string.lower("AISHA")
+
+
+// # Q2:
+// # Declare a list called "numbers" and initialize
+// #  it with a list of integers. Use the list index 
+// # to access each element and multiply it by 2. 
+// # Output the modified list.
+
+// # Q3:
+// # Declare a dictionary called "person" with properties "name",
+// #  "age" and "gender". Use a conditional statement to 
+// # check if the age property is greater than 30, then 
+// # output the key-value pairs of the dictionary using the 
+// # dictionary properties.
